a003bbc4c33483add266bda8738566e6435d31f6
Matheus Barros <89200586+S3EMi@users.noreply.github.com>
2021-08-20 09:52:08 -0300

    Update Bhaskara's Formula.py

--- Bhaskara's Formula.py
+++ Bhaskara's Formula.py
@@ -3,31 +3,41 @@
 
 import math as mt
 
-def pause():
-    input("\nPressione ENTER para continuar")
+def restart():
+    def gotor():
+        if pause() == "":
+            restart()
+        else:
+            restart()
 
+    def pause():
+        input("\nPressione ENTER para continuar...")
 
-a = float(input("Primeiro número (a): "))
-b = float(input("Segundo número (b): "))
-c = float(input("Terceiro número (c): "))
+    a = input("Primeiro número (a): ")
+    b = input("Segundo número (b): ")
+    c = input("Terceiro número (c): ")
+    try:
+        float(a), float(b), float(c)
+    except ValueError:
+        print("Um ou mais valores dados não foram identificados")
+        gotor()
 
-try:
-    float(a), float(b), float(c)
-except ValueError:
-    print("Um ou mais valores dados não foram identificados")
-    pause()
-    exit()
+    a = float(a)
+    b = float(b)
+    c = float (c)
+    disc = (b**2 - 4*a*c)
+    try:
+        res1 = (-b - mt.sqrt(disc))/(2*a)
+        res2 = (-b + mt.sqrt(disc))/(2*a)
+    except ZeroDivisionError:
+        print("Erro: não é possível dividir por 0")
+        gotor()
+    except ValueError:
+        print("Erro de domínio matemático")
+        gotor()
 
-disc = (b**2 - 4*a*c)
-try:
-    res1 = (-b - mt.sqrt(disc))/(2*a)
-    res2 = (-b + mt.sqrt(disc))/(2*a)
-except (ZeroDivisionError, ValueError):
-    print("Erro de domínio matemático")
-    pause()
-    exit()
+    print("\nx' = ", res1)
+    print("x'' = ", res2)
+    gotor()
 
-print("\nx' = ", res1)
-print("x'' = ", res2)
-
-pause()
+restart()
